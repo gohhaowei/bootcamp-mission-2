60814a65d526f692a558b84e5c474c22b10a46e5
gohhaowei <gohhaowei@gmail.com>
2020-04-15 02:06:26 +0800

    Completed Mission 2

--- src/main/java/com/btkbootcamp/mission2/Car.java
+++ src/main/java/com/btkbootcamp/mission2/Car.java
@@ -1,24 +1,22 @@
 package com.btkbootcamp.mission2;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import java.util.UUID;
 
-@Getter @Setter @NoArgsConstructor
+@NoArgsConstructor @Data
 
 public class Car {
 
-    private UUID id;
-    private String brand;
-    private String model;
-    private String year;
+    int id; // Stores the id number of each Car (this will be a running number)
+    private String brand; // Brand of the car (e.g. Toyota)
+    private String model; // Model of the car (e.g. Camry)
+    private String year; // Year of the car (e.g. 2020)
 
     public Car (String brand, String model, String year){
         this.brand = brand;
         this.model = model;
         this.year = year;
-        this.id = UUID.randomUUID();
 
     }
+
 }
--- src/main/java/com/btkbootcamp/mission2/CarController.java
+++ src/main/java/com/btkbootcamp/mission2/CarController.java
@@ -1,34 +1,70 @@
 package com.btkbootcamp.mission2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Book;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional.*;
 
 @RestController
 @RequestMapping("/api/cars")
 
 public class CarController {
 
+    int id = 1; // Sets the initial id number (running number)
+    ArrayList<Car> listOfCars = new ArrayList<Car>(); // This will store the list of cars created
 
-
-    ArrayList<Car> listOfCars = new ArrayList<Car>();
-
+    // Return all cars created
     @GetMapping
-    public Iterable findAll(){
+    public Iterable getAllCars(){
         return listOfCars;
     }
 
+    // Return only the car with the matching id
+    @GetMapping ("/{id}")
+    public Car getOneCar(@PathVariable int id){
+        for (Car car : listOfCars){
+            if(car.getId() == id){
+                return car;
+            }
+        }
+        return null;
+    }
+
+    // Create a new car
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Car create(@RequestBody Car car){
+        car.setId(id++); // This is where id is incremented for every new car created
         listOfCars.add(car);
         return car;
     }
 
+    // Delete the car with the matching id
+    @DeleteMapping ("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id){
+        // Search through the ArrayList of cars. If the current car has
+        // matching id, then this car is removed.
+        for (Car car : listOfCars) {
+            if (car.getId() == id) {
+                listOfCars.remove(car);
+                return;
+            }
+        }
+    }
 
+    // Update the car with the matching id
+    @PutMapping ("/{id}")
+    public Car update(@PathVariable int id, @RequestBody Car newCar) {
+        // Search through the ArrayList of cars. If the current car has
+        // matching id, then fetch the index of the car that was found.
+        // Then, replace it with the new car object from the Request Body.
+        for (Car car : listOfCars) {
+            if (car.getId() == id){
+                newCar.setId(id);
+                listOfCars.set(listOfCars.indexOf(car), newCar);
+                return newCar;
+            }
+        }
+        return null;
+    }
 }
